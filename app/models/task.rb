c8e69b13dd53f475955cab5e97a773d152a9d7a6
Ricardo Mejía <mejiaro@gmail.com>
2021-04-19 15:16:16 -0600

    Added validation of task name

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :list
 
+  validates :name, presence: true
+
   scope :pending_first, -> { order(completed_at: :desc) }
-  
+ 
   def completed?
     completed_at.present?
   end
